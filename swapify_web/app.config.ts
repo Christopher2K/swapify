db6433e1566b7c4e88924df2adf0118709d810a6
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-06-09 20:57:01 -0400

    feat: introduce the frontend app

---
+++ swapify_web/app.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@solidjs/start/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  vite: {
+    plugins: [tsconfigPaths()],
+  },
+});
