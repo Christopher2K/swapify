b72dbc6b3667afb4b96901b8da7969c0ac9d2317
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-06-11 08:50:06 -0400

    fix: use h3 built in proxying

--- swapify_web/src/routes/api/[...proxy].tsx
+++ swapify_web/src/routes/api/[...proxy].tsx
@@ -1,31 +1,25 @@
-import { createProxyEventHandler } from "h3-proxy";
 import { APIEvent } from "@solidjs/start/server";
+import { sendProxy } from "vinxi/http";
 
 import { useSession } from "#root/services/session";
 
-const proxy = createProxyEventHandler({
-  target: import.meta.env.VITE_API_URL,
-  enableLogger: true,
-  configureProxyRequest(event) {
-    const mbToken = event.context["accessToken"];
-    if (mbToken) {
-      return {
-        headers: {
-          Authorization: "Bearer " + mbToken,
-        },
-      };
-    }
-    return {};
-  },
-});
-
 async function handler(event: APIEvent) {
   const session = await useSession(event.nativeEvent);
+  let headers: Record<string, string> = {};
   if (session.data.auth) {
-    event.nativeEvent.context["accessToken"] = session.data.auth.accessToken;
+    headers["Authorization"] = `Bearer ${session.data.auth.accessToken}`;
   }
 
-  return proxy(event.nativeEvent);
+  return sendProxy(
+    event.nativeEvent,
+    process.env.VITE_API_URL + new URL(event.request.url).pathname,
+    {
+      fetchOptions: {
+        redirect: "manual",
+      },
+      headers,
+    },
+  );
 }
 
 export const GET = handler;
