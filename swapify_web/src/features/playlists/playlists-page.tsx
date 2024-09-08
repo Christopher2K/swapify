d1ca7266f83e1f56eaeab7c62f1acbb90b19473e
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-09-08 17:55:44 -0400

    feat: playlist page design adjustments

--- swapify_web/src/features/playlists/playlists-page.tsx
+++ swapify_web/src/features/playlists/playlists-page.tsx
@@ -1,38 +1,45 @@
 import { useEffect } from "react";
 
 import { useScreenOptions } from "#root/components/app-screen-layout";
+import { Heading } from "#root/components/ui/heading";
 import { VStack } from "#style/jsx";
 
 import { PlaylistsTable } from "./components/playlists-table";
-import { usePlaylistSyncSocket } from "./hooks/use-playlist-sync-socket";
 import { useLibrariesQuery } from "./hooks/use-libraries-query";
 
 export function PlaylistsPage() {
   const { setPageTitle } = useScreenOptions();
-  const { sendMessage } = usePlaylistSyncSocket();
   const { libraries } = useLibrariesQuery();
-
-  function syncAppleMusic() {
-    sendMessage("sync", {
-      playlistId: "library",
-      platformName: "applemusic",
-    });
-  }
-
-  function syncSpotify() {
-    sendMessage("sync", {
-      playlistId: "library",
-      platformName: "spotify",
-    });
-  }
 
   useEffect(() => {
     setPageTitle("Playlists");
   }, []);
 
   return (
-    <VStack w="full" h="full" p="4">
-      <PlaylistsTable playlists={libraries} />
+    <VStack w="full" h="full" p="4" gap="10">
+      <VStack
+        w="full"
+        gap="4"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <Heading as="h2" size="xl">
+          Libraries
+        </Heading>
+        <PlaylistsTable playlists={libraries} />
+      </VStack>
+
+      <VStack
+        w="full"
+        gap="4"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <Heading as="h2" size="xl">
+          Playlists
+        </Heading>
+        <p>Coming soon...</p>
+      </VStack>
     </VStack>
   );
 }
