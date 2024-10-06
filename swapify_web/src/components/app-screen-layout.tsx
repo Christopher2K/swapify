724a2b267deda31ad5daaf732f9188d9b215c9a0
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-10-06 14:57:44 -0400

    feat: layout adjustments

--- swapify_web/src/components/app-screen-layout.tsx
+++ swapify_web/src/components/app-screen-layout.tsx
@@ -5,11 +5,12 @@ import {
   useEffect,
   createContext,
 } from "react";
-import { Logs } from "lucide-react";
+import { XIcon, Logs } from "lucide-react";
 
 import { HStack, VStack, Box } from "#style/jsx";
 
 import { Button } from "./ui/button";
+import { Text } from "./ui/text";
 import { Sidebar } from "./sidebar";
 import { Heading } from "./ui/heading";
 import { useRouter } from "@tanstack/react-router";
@@ -33,6 +34,8 @@ export function AppScreenLayout({ children }: AppScreenLayoutProps) {
   const [pageTitle, setPageTitle] = useState("");
   const router = useRouter();
 
+  const toggleMobileMenu = () => setIsSidebarMobileOpen((isOpen) => !isOpen);
+
   useEffect(() => {
     router.subscribe("onBeforeNavigate", () => {
       setIsSidebarMobileOpen(false);
@@ -41,70 +44,110 @@ export function AppScreenLayout({ children }: AppScreenLayoutProps) {
 
   return (
     <ScreenOptionsContext.Provider value={{ setPageTitle }}>
-      <HStack
-        /* @ts-expect-error */
-        as="main"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        w="full"
-        gap="0"
-        position="relative"
-      >
-        <Sidebar
-          navProps={{ flexShrink: "0" }}
-          isMobileOpen={isSidebarMobileOpen}
-          onMobileClose={() => setIsSidebarMobileOpen(false)}
-        />
-        <VStack
-          flex="1"
+      {/* @ts-expect-error */}
+      <VStack height="100svh" width="full" as="main" gap="0">
+        <HStack
+          /* @ts-expect-error */
+          as="nav"
+          h="64px"
           w="full"
-          minH="100svh"
-          gap="0"
-          pl={[undefined, "300px"]}
-          justifyContent="flex-start"
-          alignItems="flex-start"
-          transition="all"
-          transitionTimingFunction="ease-in-out"
-          transitionDuration="fast"
+          px="4"
+          justifyContent="space-between"
+          borderBottom="thin"
+          borderStyle="solid"
+          borderColor="accent.4"
         >
-          <Box w="full" maxW="1100px" mx="auto">
-            <HStack
-              position="sticky"
-              top="0"
-              w="full"
-              gap="4"
-              px="4"
-              height="16"
-              bg="accent.1"
-              zIndex="banner"
-            >
-              <Button
-                display={["block", "none"]}
-                variant="outline"
-                onClick={() => setIsSidebarMobileOpen(true)}
-              >
-                <Logs />
-              </Button>
-              <Heading as="h1" size="2xl">
-                {pageTitle}
-              </Heading>
-            </HStack>
+          <Text textStyle="xl" fontWeight="bold">
+            Swapify
+          </Text>
 
-            <Box
-              position="relative"
-              minH="100%"
-              flex={1}
-              alignItems="stretch"
-              width="full"
-              bg="accent.1"
-            >
-              <Box position="absolute" height="full" width="full" zIndex="1">
-                {children}
-              </Box>
-            </Box>
+          <Button
+            display={["block", "none"]}
+            variant="outline"
+            onClick={toggleMobileMenu}
+          >
+            <Logs />
+          </Button>
+        </HStack>
+        <HStack width="100%" position="relative" flex={1} gap="0" height="100%">
+          <Sidebar
+            navProps={{ flexShrink: "0" }}
+            isMobileOpen={isSidebarMobileOpen}
+          />
+          <Box
+            position="absolute"
+            top="0"
+            left={["0", "300px"]}
+            right="0"
+            height="100%"
+            overflow="auto"
+          >
+            {children}
           </Box>
-        </VStack>
-      </HStack>
+        </HStack>
+      </VStack>
     </ScreenOptionsContext.Provider>
   );
 }
+
+// {/* <HStack */}
+// {/*   as="main" */}
+// {/*   justifyContent="flex-start" */}
+// {/*   alignItems="flex-start" */}
+// {/*   w="full" */}
+// {/*   gap="0" */}
+// {/*   position="relative" */}
+// {/* > */}
+// {/*   <VStack */}
+// {/*     flex="1" */}
+// {/*     w="full" */}
+// {/*     minH="100svh" */}
+// {/*     gap="0" */}
+// {/*     pl={[undefined, "300px"]} */}
+// {/*     justifyContent="flex-start" */}
+// {/*     alignItems="flex-start" */}
+// {/*     transition="all" */}
+// {/*     transitionTimingFunction="ease-in-out" */}
+// {/*     transitionDuration="fast" */}
+// {/*   > */}
+// {/*     <Box w="full" maxW="1100px" mx="auto"> */}
+// {/*       <HStack */}
+// {/*         position="sticky" */}
+// {/*         top="0" */}
+// {/*         w="full" */}
+// {/*         gap="4" */}
+// {/*         px="4" */}
+// {/*         height="16" */}
+// {/*         bg="accent.1" */}
+// {/*         zIndex="banner" */}
+// {/*       > */}
+// {/*         <Button */}
+// {/*           display={["block", "none"]} */}
+// {/*           variant="outline" */}
+// {/*           onClick={() => setIsSidebarMobileOpen(true)} */}
+// {/*         > */}
+// {/*           <Logs /> */}
+// {/*         </Button> */}
+// {/*         <Heading as="h1" size="2xl"> */}
+// {/*           {pageTitle} */}
+// {/*         </Heading> */}
+// {/*       </HStack> */}
+// {/**/}
+// {/*       <Box */}
+// {/*         position="relative" */}
+// {/*         minH="100%" */}
+// {/*         flex={1} */}
+// {/*         alignItems="stretch" */}
+// {/*         width="full" */}
+// {/*         bg="accent.1" */}
+// {/*       > */}
+// {/*         <Box */}
+// {/*           position="absolute" */}
+// {/*           height="full" */}
+// {/*           width="full" */}
+// {/*           zIndex="1" */}
+// {/*         ></Box> */}
+// {/*       </Box> */}
+// {/*     </Box> */}
+// {/*   </VStack> */}
+// {/* </HStack> */}
--- swapify_web/src/components/sidebar.tsx
+++ swapify_web/src/components/sidebar.tsx
@@ -1,17 +1,11 @@
 import type { ComponentProps, ReactNode } from "react";
 import { Link } from "@tanstack/react-router";
-import {
-  LayoutDashboardIcon,
-  ToyBrickIcon,
-  XIcon,
-  MusicIcon,
-} from "lucide-react";
+import { LayoutDashboardIcon, ToyBrickIcon, MusicIcon } from "lucide-react";
 
 import { VStack } from "#style/jsx";
 import { css } from "#style/css";
 
 import { Button } from "./ui/button";
-import { Text } from "./ui/text";
 
 type SidebarLinkProps = {
   to: ComponentProps<typeof Link>["to"];
@@ -25,6 +19,7 @@ const SidebarLink = ({ to, label, icon }: SidebarLinkProps) => {
       size="md"
       width="full"
       justifyContent="flex-start"
+      fontWeight="medium"
       asChild
     >
       <Link to={to} activeProps={{ className: css({ bg: "accent.3" }) }}>
@@ -38,61 +33,35 @@ const SidebarLink = ({ to, label, icon }: SidebarLinkProps) => {
 export type SidebarProps = {
   navProps?: ComponentProps<typeof VStack>;
   isMobileOpen?: boolean;
-  onMobileClose?: () => void;
 };
 
-export function Sidebar({
-  navProps,
-  isMobileOpen,
-  onMobileClose,
-}: SidebarProps) {
+export function Sidebar({ navProps, isMobileOpen }: SidebarProps) {
   return (
     <VStack
       /* @ts-expect-error */
-      as="nav"
-      position="fixed"
-      top="0"
+      as="aside"
+      position="absolute"
+      top={0}
+      left={0}
       width={["85%", "300px"]}
-      height="100svh"
+      height="100%"
       bg="accent.1"
-      borderRight="thin"
-      borderStyle="solid"
-      borderColor="accent.4"
       p="4"
       gap="4"
-      boxShadow="sm"
       zIndex="overlay"
       transition="all"
       transitionTimingFunction="ease-in-out"
       transitionDuration="fast"
+      borderRight={["thin", "none"]}
+      borderStyle="solid"
+      borderColor="accent.4"
       transform={[
         isMobileOpen ? "translateX(0)" : "translateX(-100%)",
         "translateX(0)",
       ]}
       {...navProps}
     >
-      <header
-        className={css({
-          position: "relative",
-          width: "full",
-          textAlign: "center",
-        })}
-      >
-        <Text as="h1" textStyle="xl" fontWeight="bold">
-          Swapify
-        </Text>
-        <Button
-          variant="link"
-          onClick={onMobileClose}
-          position="absolute"
-          top="0"
-          right="0"
-          display={["block", "none"]}
-        >
-          <XIcon strokeWidth="2" />
-        </Button>
-      </header>
-      <VStack flex="1" w="full" gap="4" px="4">
+      <VStack flex="1" w="full" gap="4">
         <SidebarLink
           to="/"
           label="Dashboard"
