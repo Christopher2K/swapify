51867f4ed015c9d9f35b790c0327c3066f43e9bc
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-11-06 15:23:52 -0500

    fix: remove test for commented codeblock

--- swapify_api/test/swapify_api_web/controllers/auth_controller_test.exs
+++ swapify_api/test/swapify_api_web/controllers/auth_controller_test.exs
@@ -3,28 +3,28 @@ defmodule SwapifyApiWeb.AuthControllerTest do
 
   import SwapifyApi.AccountsFixtures
 
-  describe "POST /api/auth/signup" do
-    test "it signs up a new user", %{conn: conn} do
-      conn =
-        post(conn, "/api/auth/signup",
-          email: "chris@test.fr",
-          password: "password1234",
-          username: "chris"
-        )
-
-      assert conn.status == 200
-    end
-
-    test "it fails when an user with this email already exists", %{conn: conn} do
-      email = "chris@test.fr"
-      user_fixture(%{email: email})
-
-      conn =
-        post(conn, "/api/auth/signup", email: email, password: "password1234", username: "chris")
-
-      assert conn.status == 422
-    end
-  end
+  # describe "POST /api/auth/signup" do
+  #   test "it signs up a new user", %{conn: conn} do
+  #     conn =
+  #       post(conn, "/api/auth/signup",
+  #         email: "chris@test.fr",
+  #         password: "password1234",
+  #         username: "chris"
+  #       )
+  #
+  #     assert conn.status == 200
+  #   end
+  #
+  #   test "it fails when an user with this email already exists", %{conn: conn} do
+  #     email = "chris@test.fr"
+  #     user_fixture(%{email: email})
+  #
+  #     conn =
+  #       post(conn, "/api/auth/signup", email: email, password: "password1234", username: "chris")
+  #
+  #     assert conn.status == 422
+  #   end
+  # end
 
   describe "POST /api/auth/signin" do
     setup do
