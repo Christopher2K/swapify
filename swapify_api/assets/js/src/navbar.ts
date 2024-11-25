d010f2b6721b014bd1e4e58df832dd3646fae60c
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-11-24 22:30:06 -0500

    fix: navbar js error

--- swapify_api/assets/js/src/navbar.ts
+++ swapify_api/assets/js/src/navbar.ts
@@ -1,6 +1,6 @@
 const menu = document.querySelector("#nav-menu")!;
 const toggle = document.querySelector("#menu-toggle")!;
 
-toggle.addEventListener("click", () => {
+toggle?.addEventListener("click", () => {
   menu.classList.toggle("invisible");
 });
